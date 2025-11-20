84aca909afbafa108963e8a09e06320521fc5285
tortoise <85298331+fly2tortoise@users.noreply.github.com>
2025-11-20 17:06:33 +0800

    2

--- 2_Darts_space/eval/genotypes.py
+++ 2_Darts_space/eval/genotypes.py
@@ -19,11 +19,6 @@ DARTS_V1 = Genotype(normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_con
 DARTS_V2 = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 1), ('skip_connect', 0), ('skip_connect', 0), ('dil_conv_3x3', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=[2, 3, 4, 5])
 DARTS = DARTS_V2
 
-# Training-Free NAS methods
-# 24 ICLR
-RoBoT  = Genotype(normal=[('dil_conv_3x3', 1), ('skip_connect', 0), ('dil_conv_3x3', 2), ('max_pool_3x3', 1), ('avg_pool_3x3', 0), ('sep_conv_5x5', 1), ('sep_conv_5x5', 2), ('sep_conv_5x5', 3)], normal_concat=range(2, 6), reduce=[('max_pool_3x3', 0), ('avg_pool_3x3', 1), ('avg_pool_3x3', 2), ('skip_connect', 0), ('max_pool_3x3', 2), ('max_pool_3x3', 0), ('sep_conv_5x5', 4), ('skip_connect', 3)], reduce_concat=range(2, 6))
-# 24 ICLR
-SWAP_C = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 2), ('skip_connect', 0), ('sep_conv_3x3', 2), ('sep_conv_3x3', 0), ('sep_conv_5x5', 4), ('sep_conv_5x5', 1)], normal_concat=[2, 3, 4, 5], reduce=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 2), ('skip_connect', 0), ('sep_conv_3x3', 2), ('sep_conv_3x3', 0), ('sep_conv_5x5', 4), ('sep_conv_5x5', 1)], reduce_concat=[2, 3, 4, 5])
 
 # Weight-sharing robust NAS methods
 # 20 Arxiv
@@ -42,5 +37,5 @@ CROZE  = Genotype(normal=[('avg_pool_3x3', 0), ('dil_conv_3x3', 1), ('dil_conv_5
 RNTK       = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_5x5', 1),('sep_conv_3x3', 0), ('dil_conv_5x5', 1),('skip_connect', 2), ('sep_conv_3x3', 3),('dil_conv_3x3', 2), ('skip_connect', 4)], normal_concat=[2, 3, 4, 5], reduce=[('avg_pool_3x3', 0), ('sep_conv_3x3', 1),('dil_conv_5x5', 2), ('sep_conv_3x3', 1),('avg_pool_3x3', 0), ('skip_connect', 3),('max_pool_3x3', 4), ('skip_connect', 1)], reduce_concat=[2, 3, 4, 5])
 
 # Ours (Choose two good architectures)
-TRNAS_c10      = Genotype(normal=[('sep_conv_5x5', 0), ('sep_conv_5x5', 1), ('sep_conv_5x5', 1), ('sep_conv_5x5', 2), ('skip_connect', 0), ('dil_conv_5x5', 2), ('skip_connect', 1), ('dil_conv_5x5', 3)], normal_concat=range(2, 6), reduce=[('sep_conv_5x5', 0), ('sep_conv_5x5', 1), ('sep_conv_5x5', 0), ('sep_conv_5x5', 1), ('sep_conv_3x3', 0), ('sep_conv_5x5', 3), ('sep_conv_5x5', 1), ('sep_conv_3x3', 4)], reduce_concat=range(2, 6))
+TRNAS_c10      = Genotype(normal=[('sep_conv_5x5', 0), ('sep_conv_5x5', 1), ('sep_conv_5x5', 1), ('skip_connect', 0), ('avg_pool_3x3', 1), ('sep_conv_5x5', 3), ('skip_connect', 1), ('sep_conv_5x5', 4)], normal_concat=range(2, 6), reduce=[('sep_conv_5x5', 0), ('sep_conv_5x5', 1), ('sep_conv_5x5', 0), ('sep_conv_5x5', 1), ('sep_conv_5x5', 0), ('sep_conv_5x5', 1), ('sep_conv_5x5', 1), ('sep_conv_5x5', 3)], reduce_concat=range(2, 6))
 TRNAS_imagenet = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('skip_connect', 0), ('sep_conv_5x5', 1), ('dil_conv_5x5', 3), ('sep_conv_3x3', 1)], normal_concat=range(2, 6), reduce=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 2), ('sep_conv_3x3', 0), ('sep_conv_5x5', 0),('sep_conv_5x5', 2), ('sep_conv_5x5', 4), ('sep_conv_5x5', 0)], reduce_concat=range(2, 6))
